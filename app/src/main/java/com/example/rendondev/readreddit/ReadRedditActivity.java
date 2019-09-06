0c07dc2ace2641ec1a533c3b0415a3b1f35f1de0
O-LRENDON <12345678>
2019-09-05 22:45:56 -0500

    'init'

---
+++ app/src/main/java/com/example/rendondev/readreddit/ReadRedditActivity.java
@@ -0,0 +1,13 @@
+package com.example.rendondev.readreddit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReadRedditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_reddit);
+    }
+}
